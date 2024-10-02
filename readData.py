8f242cb8bf9ff0e753571bee4c31f72e2901eeee
Nguyễn Tiến Đạt <142644969+MasEmIOT@users.noreply.github.com>
2024-10-02 22:40:00 +0700

    Update readData.py

--- readData.py
+++ readData.py
@@ -14,15 +14,4 @@ try:
 
 except KeyboardInterrupt:
     GPIO.cleanup()  # Dọn dẹp chân GPIO
--	Step 5 (thiết lập Node – Red Flow)
-•	Mở giao diện node – red : truy cập http://<IP của Raspberry Pi>:1880
-•	Tạo flow để đọc dữ liệu từ cảm biến  
-o	Node Inject: Để thử nghiệm, bạn có thể sử dụng Inject để tạo tín hiệu giả lập dữ liệu cảm biến.
-o	 Node Function: Sử dụng node này để xử lý dữ liệu. Ví dụ: nếu bạn đọc nhiệt độ và cần cảnh báo khi nhiệt độ vượt quá một mức nhất định, bạn có thể xử lý điều này tại đây.
-Ví dụ, để kiểm tra nhiệt độ
-if (msg.payload > 70) {
-    msg.payload = "Cảnh báo: Nhiệt độ quá cao!";
-} else {
-    msg.payload = "Nhiệt độ bình thường: " + msg.payload + "°C";
-}
-return msg;
+
